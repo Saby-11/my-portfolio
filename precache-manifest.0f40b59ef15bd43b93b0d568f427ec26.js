280150697c7341069c00cc48b0a4cc5b65352ce2
Saurabhh Tyagi <styagi689@gmail.com>
2020-12-02 18:10:46 +0530

    Updates

---
+++ precache-manifest.0f40b59ef15bd43b93b0d568f427ec26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "826a79f3bf8497ccef17",
+    "url": "/my-portfolio/static/css/main.35f50cf8.chunk.css"
+  },
+  {
+    "revision": "826a79f3bf8497ccef17",
+    "url": "/my-portfolio/static/js/main.826a79f3.chunk.js"
+  },
+  {
+    "revision": "2870dd7982876d03c6f9",
+    "url": "/my-portfolio/static/js/1.2870dd79.chunk.js"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "ce82e95f27d3bf5605a4201553b7c1aa",
+    "url": "/my-portfolio/index.html"
+  }
+];
